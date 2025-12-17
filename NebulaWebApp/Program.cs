66e7b64550c9e50ad71f893f3d29ac27a6888ec7
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-12-17 19:24:48 +0300

    Add swagger

--- NebulaWebApp/Program.cs
+++ NebulaWebApp/Program.cs
@@ -12,6 +12,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();
--- NebulaWebApp/obj/Debug/net9.0/NebulaWebApp.AssemblyInfo.cs
+++ NebulaWebApp/obj/Debug/net9.0/NebulaWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NebulaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a1c072fae3a8029c901c74765be357542d543d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NebulaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NebulaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
