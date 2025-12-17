4799fdfe1a922e988d400771b7b3ce8dd4a3089f
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-12-17 19:55:10 +0300

    update swagger

--- NebulaWebApp/obj/Debug/net9.0/NebulaWebApp.AssemblyInfo.cs
+++ NebulaWebApp/obj/Debug/net9.0/NebulaWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NebulaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a1c072fae3a8029c901c74765be357542d543d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e133194ac44db732a2c3a57438a70fbf98d41c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NebulaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NebulaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
